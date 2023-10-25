d0f2f1d36409e822f17959b403f3cbd9ee0fc955
Trey Marshall <marshall.trey92@yahoo.com>
2023-10-25 16:55:12 -0500

    Header updates, contact me page updates

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
       <div className="gif-background">
         <div className="bio-container">
           <p>This is Trey, I'm a software developer.</p>
+          <p>This portfolio is being used to showcase my growing technical abilities. As I continue to learn more skills and complete more projects, this will continue to be updated and maintained as a work of effort and love.</p>
         </div>
       </div>
     </main>
